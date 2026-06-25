c27fff7416b6320e3533593dc8bf5a18893de718
Mayank <151539657+01mayankk@users.noreply.github.com>
2026-06-25 10:32:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stock span problem/stock-span-problem.cpp
+++ Difficulty: Medium/Stock span problem/stock-span-problem.cpp
@@ -1,26 +1,27 @@
 class Solution {
   public:
     vector<int> calculateSpan(vector<int>& arr) {
+        // code here
+        
         int n = arr.size();
-        vector<int> span(n);          // to store result
-        stack<int> st;               // stack to store indices
-
+        
+        vector<int> ans(n);
+        
+        stack<int> st;
+        
         for (int i = 0; i < n; i++) {
-
-            // Pop elements from stack while
-            // current price is greater than or equal
-            // to price at stack top index
-            while (!st.empty() && arr[st.top()] <= arr[i]) {
+            
+            while (!st.empty() && arr[st.top()] <= arr[i])
+            {
                 st.pop();
+            
             }
-
-            // If stack is empty, price is greater than all previous prices
-            span[i] = st.empty() ? (i + 1) : (i - st.top());
-
-            // Push current index onto stack
+            
+            ans[i] = st.empty() ? (i + 1) : (i - st.top());
+            
             st.push(i);
         }
-
-        return span;
+        
+        return ans;
     }
 };
